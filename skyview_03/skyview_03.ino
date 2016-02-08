5e614280e956c04248a67d730649f225dedf45c2
Adrian Wong <adrian@almostsquare.com>
2016-02-07 19:35:22 -0800

    Blocking code with buffer overflows, but it works

--- skyview_03/skyview_03.ino
+++ skyview_03/skyview_03.ino
@@ -11,23 +11,16 @@ void setup() {
   pinMode(13, OUTPUT);  // YELLOW LED - SENDING
   
   // Setup computer to Teensy serial
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Setup Teensy to ESP8266 serial
   // Use baud rate 115200 during firmware update
-  Serial1.begin(9600);
+  Serial1.begin(115200);
 
 }
 
 void loop() {
   int c;
-  // Send bytes from ESP8266 -> Teensy to Computer
-  if ( Serial1.available() ) {
-    digitalWriteFast(13, HIGH);   // set the LED on
-    LED_13_TimeOn = millis();
-    c = Serial1.read();
-    Serial.write(c);
-  }
 
   // Send bytes from Computer -> Teensy back to ESP8266
   if ( Serial.available() ) {
@@ -37,8 +30,16 @@ void loop() {
     Serial1.write(c);
   }
 
+  // Send bytes from ESP8266 -> Teensy to Computer
+  if ( Serial1.available() ) {
+    digitalWrite(13, HIGH);   // set the LED on
+    LED_13_TimeOn = millis();
+    c = Serial1.read();
+    Serial.write(c);
+  }
+
   if (millis() - LED_13_TimeOn > 20) {
-    digitalWriteFast(13, LOW);   // set the LED off
+    digitalWrite(13, LOW);   // set the LED off
   }
 }
 
